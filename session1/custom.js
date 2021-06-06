711695e76300307980aeb24f0637911a20c318d5
marwa Radwan <marwaradwan872@hotmail.com>
2021-06-06 13:28:48 +0200

    s1

--- session1/custom.js
+++ session1/custom.js
@@ -105,28 +105,44 @@ add user object to user array
 
 // "heloo" + n +"bvgb" 
 // `hello ${bn} hgj`
-let users = []
 
-let getUserData = function(){
+// let functionName = (params) => {
+//     //body
+// }
 
-    let user = { name: null, age:null, salary:null, job:null, address:null }
 
-    userDetailsObj = Object.keys(user)
+// let users = []
 
-    userDetailsObj.forEach(element => {
+// let validateNumber = function (key, value) {
+//     while(isNaN(Number(value)) ){
+//         value = prompt(`please enter valid data for ${key}`)
+//     }
+//     value = Number(value)
+//     return value
+// }
+// let getUserData = function(){
 
-        user[element] = prompt(`enter user ${element}: `)        
+//     let user = { name: null, age:null, salary:null, job:null, address:null }
 
-    });
+//     userDetailsObj = Object.keys(user)
 
-    // console.log(user)
+//     userDetailsObj.forEach(element => {
 
-    users.push(user)
+//         user[element] = prompt(`enter user ${element}: `)       
+//         if (element=="salary"||element=='age') {
+//            user[element] = validateNumber(element, user[element])
+//         }
 
-}
+//     });
 
-getUserData()
-console.log(users)
+//     // console.log(user)
+
+//     users.push(user)
+
+// }
+
+// getUserData()
+// console.log(users)
 // name = prompt('enter your name')
 // age = prompt('enter your age')
 // salary = prompt('enter your salary')
@@ -137,31 +153,70 @@ console.log(users)
 // users.push(user)
 // console.log(users)
 
+// Objects
+
+// DOM
+
+// JSON
 
 
 
 
+//json
+// jsonData = [
+//     {
+//         "name":"marwa",
+//         "age":36
+//     }
+// ]
+
+//object
+// let user = {
+//     name: "marwa",
+//     birthYear: 1985,
+//     address: "giza",
+//     getUserData: function () {
+//         console.log(`User name is ${this.name} and his age is ${2021-this.birthYear}`);        
+//     }
+// }
+
+// user.getUserData()
+
+// let test = function(x) {
+//     return {
+//         val: x,
+//         mul: x*2
+//     }
+// }
+
+// console.log(test(5))
 
 
 
 
+// var x =[5]
+// function z(x){x[0]=3}
+// z(x)
+// console.log(x)
 
 
+let user = {
+    balnce:1000
+}
 
+addBalance = function (user) {
+    user.balnce+=500    
+}
 
+addBalance(user)
+console.log(user)
 
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+user=> name, balance, taxes
+ add balance => balance => balnce++ , taxes = balance*10%
+ display data =>print user data
+ reset account=> balance, taxes =0
+*/
 
 
 
