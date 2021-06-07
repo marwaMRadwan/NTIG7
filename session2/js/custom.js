b9eeebd0d3ce45d0a955c32ff5358c18414a064d
marwa Radwan <marwaradwan872@hotmail.com>
2021-06-07 11:48:40 +0200

    s2

--- session2/js/custom.js
+++ session2/js/custom.js
@@ -125,38 +125,7 @@ const createCustomElement = (parent, element, classes , attributes, text) => {
     }
     return myElement
 }
-const posts = [
-    {
-    userId: 1,
-    id: 1,
-    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-    userId: 1,
-    id: 2,
-    title: "qui est esse",
-    body: "est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-    userId: 1,
-    id: 3,
-    title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-    body: "et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut"
-    },
-    {
-    userId: 1,
-    id: 4,
-    title: "eum et est occaecati",
-    body: "ullam et saepe reiciendis voluptatem adipisci sit amet autem assumenda provident rerum culpa quis hic commodi nesciunt rem tenetur doloremque ipsam iure quis sunt voluptatem rerum illo velit"
-    },
-    {
-    userId: 1,
-    id: 5,
-    title: "nesciunt quas odio",
-    body: "repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est voluptatem omnis possimus esse voluptatibus quis est aut tenetur dolor neque"
-    }
-    ]
+const posts = JSON.parse(localStorage.getItem('posts'))||[]
 const parent = document.querySelector('#tableContent')
 const newTable = createCustomElement(parent, 'table', 'table table-stripped', [], '')
 const newTableHead = createCustomElement(newTable, 'thead', '', [], '')
@@ -164,11 +133,30 @@ const newTableBody = createCustomElement(newTable, 'tbody', '', [], '')
 let tr = createCustomElement(newTableHead, 'tr', '', [], '')
 const heads = ['userId', 'id', 'title', 'body']
 heads.forEach( h => th = createCustomElement(tr, 'th','', [], h))
-posts.forEach(post=>{
+if(posts.length == 0){
     tr= createCustomElement(newTableBody, 'tr', '', [], '')
-    heads.forEach( h => th = createCustomElement(tr, 'td','', [], post[h]))
-})
+     th = createCustomElement(tr, 'td','', [{attrName:'colspan',attValue:'4'}], 'no data to show')
 
+}
+else{
+    posts.forEach(post=>{
+        tr= createCustomElement(newTableBody, 'tr', '', [], '')
+        heads.forEach( h => th = createCustomElement(tr, 'td','', [], post[h]))
+    })
+    
+}
+
+
+document.querySelector('#addPost').addEventListener('submit', function(e){
+    e.preventDefault()
+    let newPost = {}
+    heads.forEach(h=> newPost[h] = this.elements[h].value )
+    posts.push(newPost)
+    localStorage.setItem('posts',JSON.stringify(posts))
+    tr= createCustomElement(newTableBody, 'tr', '', [], '')
+    heads.forEach( h => th = createCustomElement(tr, 'td','', [], newPost[h]))
+    this.reset()
+})
 
 
 
