ccadaa62fcf5ad2fb196517a123eaf3885f3d3a3
marwa Radwan <marwaradwan872@hotmail.com>
2021-06-16 13:12:02 +0200

    s9

---
+++ session9/dbHBS/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const userRoutes = require('../routes/users.routes')
+
+const app = express()
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, '../public')))
+hbs.registerPartials(path.join(__dirname, '../frontend/layouts'))
+app.set('views', path.join(__dirname, '../frontend/views'))
+
+app.use(userRoutes)
+module.exports = app
