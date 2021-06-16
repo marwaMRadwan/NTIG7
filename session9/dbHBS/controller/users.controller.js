4509bb1b48bed6590d3a5361c616b81d9d3e941b
marwa Radwan <marwaradwan872@hotmail.com>
2021-06-16 14:58:07 +0200

    s9

--- session9/dbHBS/controller/users.controller.js
+++ session9/dbHBS/controller/users.controller.js
@@ -1,4 +1,37 @@
 const dbConnection = require('../db/db')
+const {ObjectID}=require('mongodb')
+showAll = (req,res)=>{
+    dbConnection(db=>{
+        if(!db) return console.log('error')
+        db.collection('user').find().toArray((err, users)=>{
+            if(err)console.log(err)
+            else {
+                console.log(users)
+                res.render('all', {users})
+            }
+        })
+    })
+}
+
+showSingle = (req,res) =>{
+    let id = req.params.id
+    dbConnection(db=>{
+        db.collection('user').findOne({_id:new ObjectID(id)}, (err, user)=>{
+            res.render('single', {user})
+        })
+    })
+}
+
+deleteUser = (req,res) =>{
+    let id = req.params.id
+    dbConnection(db=>{
+        db.collection('user').deleteOne({_id:new ObjectID(id)})
+        .then(data=>{res.redirect('/')})
+        .catch(e=>{})
+    })
+}
+
+
 addNewUserView = (req, res)=>{
     res.render('add')
 }
@@ -15,5 +48,8 @@ addNewUserPOST = (req,res)=>{
 }
 module.exports={
     addNewUserView,
-    addNewUserPOST
+    addNewUserPOST,
+    showAll,
+    showSingle,
+    deleteUser
 }
--- session9/dbHBS/routes/users.routes.js
+++ session9/dbHBS/routes/users.routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = new express.Router()
 const userController = require('../controller/users.controller')
-router.get('', (req,res)=> res.send('env'))
+router.get('', userController.showAll)
+router.get('/single/:id', userController.showSingle)
+router.get('/delete/:id', userController.deleteUser)
 
 router.get('/addUser', userController.addNewUserView)
 router.post('/addUser', userController.addNewUserPOST)
+
+router.get('*', (req,res)=>{res.send('404')})
 module.exports = router
